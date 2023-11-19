2cd0c3de4a604a7a010a764ca2cc3ec425c1eb89
mountain-ha <mountain-ha@naver.com>
2023-11-19 17:23:45 +0900

    week11 class2

--- challenge/week11/student_grade.py
+++ challenge/week11/student_grade.py
@@ -1,33 +1,32 @@
     
-class  Student(): #학생 이름과 성적 클래스 생성
-    name = ""
-    korean = 0
-    math = 0
-    eng = 0
+class Student():
+    def __init__(self, name, korean, math, eng): #생성자
+        self.name = name
+        self.korean = korean
+        self.math = math
+        self.eng = eng
+
     def  get_average(self): #성적 평균을 구하는 함수
         return (self.korean+self.math+self.eng)/3
     
-def load_data(file_name):
+def load_data(file_name): #파일 열기
     fp = open(file_name, "r", encoding="utf8")
     lines = fp.readlines()
     fp.close()
 
     student_list = []
     for line in lines[1:]:
-        tokens = line.replace("\n","").split(",")
-        std_ins = Student()
-        std_ins.name = tokens[0]
-        std_ins.korean = float(tokens[1])
-        std_ins.math = float(tokens[2])
-        std_ins.eng = float(tokens[3])
-        student_list.append(std_ins)
+        tokens = line.replace("\n", "").split(",")
+        student1 = Student(tokens[0], float(tokens[1]), float(tokens[2]), float(tokens[3])) #객체 만들기
+        student_list.append(student1) #만든 객체를 리스트에 저장
+
     return student_list
 
 
 # TODO 2: 각 학생별 평균 점수 계산
 # getAverage()를 사용하여 평균 계산
-student_list = load_data("student.csv")
+student_list = load_data("student.csv") #위의 함수 작동
 for student_ins in student_list:
-    print(f"{student_ins.name}의 평균점수 {student_ins.get_average()}")
+    print(f"{student_ins.name}의 평균점수 {student_ins.get_average()}") #평균점수 출력
 
 
